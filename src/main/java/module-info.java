eebbc5a28721449d1d16393037bee6724dabd929
Leandro Afonso <leandro.sant.afonso@gmail.com>
2024-07-23 14:12:28 +0100

    updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.texttospeechjavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.jdi;
+    requires freetts;
 
 
     opens org.example.texttospeechjavafx to javafx.fxml;
